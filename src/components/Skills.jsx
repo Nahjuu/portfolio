7dfd79ca0fc6819431b528db042862b0ee96d341
Nahuel Silva <nahuelsilvanssi@gmail.com>
2026-09-17 15:26:17 +0000

    Added Skills.jsx component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
-function Skills(){
-    return(
-        <>
-        Skills
-        </>
-    )
-}
+const Skills = () => {
+  return (
+   
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          
+          <div className='w-full flex justify-center items-center flex-col mb-7'>
+              <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center'>Skills</p>
+              <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
+          </div> 
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>JavaScript</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>React</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Node</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Express</p>
+              </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>PostgreSQL</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Python</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Django</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Sqlite</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
